9f8b8966349b0aca7917e8ede9ca51f21466d123
Your Name <ju679935@gmail.com>
2022-10-12 21:49:49 +0600

    quiz quantity

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Quize.css'
 const Quiz = ({ quiz}) => {
-    const { id ,name, logo,} =quiz
+    const { id, total,name, logo,} =quiz
     return (
         <div className='quiz-container'>
            <div>
@@ -10,6 +10,7 @@ const Quiz = ({ quiz}) => {
            </div>
            <div className="quiz-details">
             <h4>{name} </h4>
+                <h5>Quiz:{total} </h5>
                 <button><Link to={`/quiz/${id}`}> Go to learn</Link> </button>
            </div>
         </div>
--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Quiz/Quize.css'
 const Chart = ({defaultChart}) => {
-    const { id, name, logo } = defaultChart
-
+    const { id,total, name, logo } = defaultChart
+console.log(defaultChart);
     return (
         <div className='quiz-container'>
             <div>
@@ -11,6 +11,7 @@ const Chart = ({defaultChart}) => {
             </div>
             <div className="quiz-details">
                 <h4>{name} </h4>
+                <h5>Quiz:{total} </h5>
                 <button><Link to={`/quiz/${id}`}> Go to learn</Link> </button>
             </div>
         </div>
